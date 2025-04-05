19b833a3ad6e9be8596636df76ff1f97dd636bbe
elgoodguy <pgzzd96@gmail.com>
2025-04-05 16:28:48 -0600

    feat(ui): add Input and Label components

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -4,4 +4,6 @@ import './styles.css';
 // Exporta componentes aquí
 export * from './components/ui/button';
 export * from './components/ui/dialog';
+export * from './components/ui/input';
+export * from './components/ui/label';
 // export * from './MyComponent'; // Ejemplo componente propio 
